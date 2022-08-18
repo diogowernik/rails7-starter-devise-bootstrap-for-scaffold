7b3e4ab40ead1a360249db1ce2f2930658ffbc0e
diogowernik <11983260+diogowernik@users.noreply.github.com>
2022-08-17 23:18:11 -0300

    Adding fix to devise erros message on registration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,4 +1,20 @@
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    else
+      super
+    end
+  end
+
+  def skip_format?
+    %w(html turbo_stream */*).include? request_format.to_s
+  end
+end
+
 Devise.setup do |config|
+
+  config.parent_controller = 'TurboDeviseController'
 
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
@@ -25,5 +41,9 @@ Devise.setup do |config|
   config.sign_out_via = :delete
 
   config.navigational_formats = ['*/*', :html, :turbo_stream]
+
+  config.warden do |manager|
+    manager.failure_app = TurboFailureApp
+  end
 
 end
